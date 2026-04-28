23ea296cfeb721765b02eaff0018f2f0700bc046
Oliver Oehme <oliver.oehme@gmail.com>
2026-04-28 21:28:55 +0200

    Fix task detail render loop and add app error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { AppErrorBoundary } from './components/AppErrorBoundary'
 import './styles.css'
 import 'leaflet/dist/leaflet.css'
 
@@ -11,9 +12,11 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AppErrorBoundary>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AppErrorBoundary>
     </QueryClientProvider>
   </React.StrictMode>,
 )
